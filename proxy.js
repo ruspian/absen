265dec6fb938087de3f105c143cd8990ef34e962
ruspian <ruspianntb@gmail.com>
2025-11-15 14:35:29 +0800

    perbaikan proxy

--- proxy.js
+++ proxy.js
@@ -1,8 +1,6 @@
 import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
-export const runtime = "nodejs";
-
 export default auth((req) => {
   const { nextUrl } = req;
   const pathname = nextUrl.pathname;
